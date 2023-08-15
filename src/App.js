a565a97cebf67dc8600051936742e65df29d1900
Muskan <muskan@darukamuskan@gmail.com>
2023-08-15 15:35:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Form from './components/Form';
+import { useState } from 'react';
+import TodoList from './components/TodoList';
+
+function App() {
+  const [todo, setTodo] = useState('')
+  const [todoList, setTodoList] = useState([])
+ 
+  return (
+    <div>
+      
+      <Header/>
+      <Form todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList}/>
+      <TodoList setTodoList={setTodoList} todoList={todoList} />
+    </div> 
+  );
+}
+
+export default App;
