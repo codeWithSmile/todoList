bc58bef5ea3dcbdc0d87a5ee0e6976765286a82c
Muskan <muskan@darukamuskan@gmail.com>
2023-08-18 18:14:28 +0530

    feat: validation on onClick

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,25 @@
 import styles from '../style.module.css'
 import shortid from 'shortid'
+import { useState } from 'react';
 
 const Form = ({ todo, setTodo, todoList, setTodoList }) => {
+    const [error, setError] = useState('');
     const handleChange=(event) => {
         setTodo(event.target.value)
     }
     const handleSubmit=(event) => {
         event.preventDefault();
+        if (!todo.trim()) {
+            setError('Please enter a todo item.');
+            return;
+        }
         setTodoList([...todoList,{name:todo, id:shortid.generate()}])
         setTodo('');
+        setError('');
     }
     return (
         <div className={styles.todoform}>
+            {error && <p className={styles.error}>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <input
                     value={todo}
